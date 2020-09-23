c370b897ee2d1f6f8ae37bbc113e320afc9ba5f2
Ogechukwu Onyejekwe <ogesmasherbaby@gmail.com>
2020-09-23 07:00:49 -0700

    modified my design

--- App.js
+++ App.js
@@ -18,15 +18,21 @@ const App = () => {
     <Text style = {style.txt1}>Skip</Text>
     <Image style = {style.image} source = {pics}/>
     <Text style ={style.txt2}>Loan</Text>
-    <Text style ={style.txt3}>You have quick, easy access to our growth loans 
-      so that you can seize the opportunity of a lifetime</Text>
+    <Text style ={style.txt3}>{`You have quick, easy access to our growth
+    loans so that you can seize the opportunity of 
+    a lifetime`}</Text>
       <TouchableOpacity style ={style.btn1}>
         <Text style={style.btntxt}>Sign up</Text>
       </TouchableOpacity>
       <TouchableOpacity style={style.btn2}>
         <Text style={style.btntxt}>Log in</Text>
       </TouchableOpacity>
+      <View style = {style.btnview}>
       <TouchableOpacity style={style.btn}/>
+      <TouchableOpacity style={style.btn}/>
+      <TouchableOpacity style={style.btn}/>
+      <TouchableOpacity style={style.btn}/>
+      </View>
   </View> 
   );
 };
@@ -60,22 +66,28 @@ const style = StyleSheet.create({
  },
  txt3: {
    paddingHorizontal: 25,
-   fontSize: 18,
+   fontSize: 15,
    fontWeight: "normal",
-   alignSelf: "center"
+   display: "flex",
+   justifyContent: "space-around",
+   textAlign: "center"
    
  },
 
  btn1: {
-   backgroundColor: "orange",
+  borderColor: "purple",
+  borderWidth: 1,
+  backgroundColor: "purple",
    padding: 15,
    borderRadius: 70,
    marginHorizontal: 25,
-   marginTop: 50
+   marginTop: 40
  },
 
  btn2: {
-   backgroundColor: "purple",
+   //backgroundColor: "purple",
+   borderColor: "purple",
+   borderWidth: 1,
    padding: 15,
    borderRadius: 70,
    marginHorizontal: 25,
@@ -84,19 +96,26 @@ const style = StyleSheet.create({
 
  btntxt: {
   textAlign: "center",
-  color: "white",
+  color: "black",
   fontSize: 15
  },
  btn: {
-   borderColor: "orange",
-   borderWidth: 5,
+   borderColor: "purple",
+   borderWidth: 1,
    borderRadius: 300,
-  alignSelf: "center",
    width: 10,
    height: 10,
+   margin: 1
+   
+  
+ },
+ btnview: {
+   display: "flex", 
+   flexDirection: "row",
    position: "absolute", 
    bottom: 25,
-  
+   alignSelf: "center"
+   
  }
 })
 export default App;
